5a4074dc03b39559fd336c7a09aa28b2522a8ea3
Amran Hossain <amrancse930@gmail.com>
2022-06-15 19:18:02 +0700

    Array

--- src/main/java/com/amran/solution/IntersectionOfTwoArrays.java
+++ src/main/java/com/amran/solution/IntersectionOfTwoArrays.java
@@ -4,6 +4,9 @@
  */
 package com.amran.solution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author Amran Hossain
@@ -15,6 +18,25 @@ public class IntersectionOfTwoArrays {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
     }
     
 }
